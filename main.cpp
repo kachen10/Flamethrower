6388aa8af970dd4740aee60c8f8597564e6325bf
Anya <ania.leonova@gmail.com>
2019-02-09 09:34:47 -0800

    Init the main file

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 # include <ncurses.h>
 
 int main( int argc, char* arg[] ) {
-
+	
+	initscr();
+	//"stdscr" to refer to the current screen
+	
+	
+	//FUNCTIONS THAT WILL GO HERE
+	
+	
+	endwin(); //bye
+	return 0;
 }
