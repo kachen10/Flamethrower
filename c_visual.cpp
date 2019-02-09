f7d5514da47b1a60389b784b1af58cbca068bab8
Anya <ania.leonova@gmail.com>
2019-02-09 13:56:07 -0800

    draw bob

--- c_visual.cpp
+++ c_visual.cpp
@@ -1,17 +1,50 @@
 #include <ncurses.h>
+#include <unistd.h>
 
 //**************FUNCTION TO DRAW BOB*******************//
 void bob_stage1(int height, int width, int size, WINDOW * win) {
 
 	//width = columns, height = rows
 	int pos; //current position in the image
+	int eye1 = 5*width+8;
+	int eye2 = 5*width+10;
+	int mouth = 5*width + 9;
 	
 	char bob[size] = {};
-	for (int i=0; i < size; i++) {
+	
+	
+	//PREPARE BOB
+	for (int i=0; i <size; i++) {
 		bob[i] = ' ';
-		//addch(bob[i]);
 	}
 	
+	for (int x=0; x < width; x++) {
+	
+		for(int y=0; y < height; y ++) {
+			pos = y*width + x;
+			
+			if(y == 3 || y == 6) {
+				if (x >= 6 && x <= 12) {
+			 		bob[pos] = '=';
+			 	} 
+			} 	
+			else if(y == 4 | y == 5) {
+				if ( x == 5 | x == 13) {
+					bob[pos] = '|';
+				}
+			} 
+			
+		}
+	
+	}
+	
+	int flag = 1;
+	bob[eye1] = '0';
+	bob[eye2] = '0';
+	bob[mouth] = '.';
+ 		
+ 		
+ 	//PRINT BOB
 	for(int x=0; x < width; x++) {
 		
 		for(int y=0; y < height; y++) {
@@ -19,8 +52,7 @@ void bob_stage1(int height, int width, int size, WINDOW * win) {
 			mvaddch(y, x, bob[pos]);
 		}
 	} 
-	
-	wrefresh(win);
+ 		
 }
 
 
@@ -46,13 +78,11 @@ int c_visual () {
 	WINDOW * c_screen = newwin(height, width, start_y, start_x);
 	refresh();
 	
-	//draw a box
-	wborder(c_screen, left, right, top, bottom, tlc, trc, blc, brc);
-	//mvwprintw(c_screen, 1, 1, "this is my box");
 	wrefresh(c_screen);
 	
 	//draw bob (stage 1)
 	bob_stage1(height, width, size, c_screen);
+	wborder(c_screen, left, right, top, bottom, tlc, trc, blc, brc);
 	
 	getch();
 	getch();
