14dd94f34993303ede7b27fc57b426ba92ad0971
Yuan Chen <yca316@sfu.ca>
2019-02-09 16:41:39 -0800

    update

--- main.cpp
+++ main.cpp
@@ -7,17 +7,17 @@
 int main( int argc, char* arg[] ) {
 
 	int max_y, max_x;
-	
+
 	initscr();
 	//"stdscr" to refer to the current screen
 
 	refresh();
 	getmaxyx(stdscr, max_y, max_x);
 	//FUNCTIONS THAT WILL GO HERE
-	
+
 	c_visual(max_x, max_y);
 	menu(max_x, max_y);
-	
+
 	refresh();
 
 	endwin(); //bye
